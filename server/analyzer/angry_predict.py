7d8de58b6981b7d6e44dc752fd1605fc72ff413b
zonozono32 <zonocat32@gmail.com>
2017-07-13 22:50:39 +0900

    見た目と本質の怒りの判定を固定値による比率で求めるように変更

--- server/analyzer/angry_predict.py
+++ server/analyzer/angry_predict.py
@@ -8,13 +8,18 @@ from numpy.random import *
 
 # 怒りを判定
 def angry_predict(heartrate, myo, myoEnable, face, faceEnable, voice, voiceEnable):
-    bodyInput = np.array([[heartrate, myo, myoEnable]])
-    lookInput = np.array([[face, voice]])
+    # bodyInput = np.array([[heartrate, myo, myoEnable]])
+    # lookInput = np.array([[face, voice]])
+    #
+    # bodyResult = body.predict(bodyInput)
+    # lookResult = look.predict(lookInput)
 
-    bodyResult = body.predict(bodyInput)
-    lookResult = look.predict(lookInput)
+    # return bodyResult[0][0], lookResult[0][0]
 
-    return bodyResult[0][0], lookResult[0][0]
+    bodyResult = heartrate * 0.5 + myo * 0.5 * myoEnable
+    lookResult = face * 0.7 * faceEnable + voice * 0.3 * voiceEnable
+
+    return bodyResult, lookResult
 
 if __name__ == '__main__':
     bodyResult, lookResult = angry_predict(0.2, 0.3, True, 0.7, True, 0.8, True)
